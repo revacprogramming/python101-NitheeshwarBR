2d4b2cf0b05e7322b3f3c2eb9d689a298f1a8530
NitheeshwarBR <nitheeshwar.r.g@gmail.com>
2022-05-13 21:37:45 +0530

    p10-dictionaries(13/5/2022)

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,4 +1,24 @@
 # Dictionaries
+filename = "dataset/mbox-short.txt"
+handle = open(filename)
 
-# filename = "dataset/mbox-short.txt"
+lines = list()
+
+for line in handle:
+    if not line.startswith("From:"): continue
+    line = line.split()
+    lines.append(line[1])
+
+counts = dict()
+for word in lines:
+    counts[word] = counts.get(word,0) + 1
+
+x = 0
+y = 0
+for word,count in counts.items(): 
+    if x ==0 or count > x:
+        x = count
+        y = word
+
+print (y,x)
 
--- ActivitySet#1/problem#22.py
+++ ActivitySet#1/problem#22.py
@@ -1,7 +1,14 @@
-a=[1,2,2,3]
-b=[5,6,7,8]
-sum=list()
-for i in a:
-    for j in b:
-        sum=a[i]+b[j]
-print(sum)
+file=open('mbox-short.txt')
+lines=list()
+count=0
+for line in file:
+    if line.startswith('From:'):
+        continue
+    elif line.startswith('From'):
+        count=count+1
+        lines=line.split()
+        words=lines[1]
+        print(words)
+        
+            
+print("There were {} lines in the file with From as the first word".format(count))
