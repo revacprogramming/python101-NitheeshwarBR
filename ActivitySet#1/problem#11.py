ca4e2f3643570246d8c15111dde4a263eb2ccba0
NitheeshwarBR <nitheeshwar.r.g@gmail.com>
2022-05-15 20:45:45 +0530

    p11,py  (15/5/2022)

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -5,10 +5,30 @@
 # From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
 # Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
 filename = "dataset/mbox-short.txt"
-file=open(filename)
+handle = open(filename)
+
 lines=list()
-count=0
-for line in file:
-    if line.startswith('From'):
-        line.split()
+for line in handle:
+    if line.startswith("From:"):
+        continue
+    elif line.startswith("From"):
+        words=line.split()
+        words=lines.append(words[5])
+    elif not line.startswith("From"):
+        continue
         
+x=list()
+for letters in lines:
+    letter=letters.split(':')
+    letter=x.append(letter[0])
+x.sort()
+hours=dict()
+for hrs in x:
+    if hrs not in hours:
+        hours[hrs]=1
+    else:
+        hours[hrs]+=1
+result=hours.items()
+
+for hr,c in result:
+    print(hr,c)
